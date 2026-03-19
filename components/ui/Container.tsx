413748ea2dfa2210635a9683a74a4a79a9f7842f
jono-au <jonathan@applify.com.au>
2026-03-20 01:13:53 +1000

    update container to accept className, id, onclick style etc

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,6 +1,19 @@
-export default function Container({ children }: { children: React.ReactNode }) {
+import type { ReactNode, HTMLAttributes } from "react";
+
+type ContainerProps = {
+  children: ReactNode;
+} & HTMLAttributes<HTMLDivElement>;
+
+export default function Container({
+  children,
+  className = "",
+  ...props
+}: ContainerProps) {
   return (
-    <div className="mx-auto w-full max-w-[1440px] px-6 lg:px-10">
+    <div
+      className={`mx-auto w-full max-w-[1440px] px-6 lg:px-10 ${className}`}
+      {...props}
+    >
       {children}
     </div>
   );
