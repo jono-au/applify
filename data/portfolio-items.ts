ab716252c57e2e84d2b8e9563d825f58278972bd
jono-au <jonathan@applify.com.au>
2026-03-31 21:43:23 +1000

    update tags

--- data/portfolio-items.ts
+++ data/portfolio-items.ts
@@ -11,7 +11,7 @@ export const portfolioLeftItems: PortfolioItem[] = [
   {
     title: "Business Chamber Queensland",
     image: "/images/portfolio/website-design.jpg",
-    tags: ["Responsive", "Design"],
+    tags: ["WordPress", "Responsive", "Integrations"],
     href: "/portfolio/innovative-website-design",
     wrapperClassName: "md:max-w-[88%]",
     imageClassName: "aspect-[1.03/0.9]",
@@ -19,7 +19,7 @@ export const portfolioLeftItems: PortfolioItem[] = [
   {
     title: "Elemeno",
     image: "/images/portfolio/toystory.jpeg",
-    tags: ["Dashboard", "UI Design"],
+    tags: ["Shopify", "Ecommerce"],
     href: "/portfolio/smart-dashboard-design",
     wrapperClassName: "md:max-w-[104%]",
     imageClassName: "aspect-[1.16/1]",
@@ -30,7 +30,7 @@ export const portfolioRightItems: PortfolioItem[] = [
   {
     title: "Minn Kota",
     image: "/images/portfolio/minnkota.png",
-    tags: ["Logo", "Strategy"],
+    tags: ["WooCommerce", "Custom Theme", "User Experience"],
     href: "/portfolio/brand-identity-makeover",
     wrapperClassName: "md:ml-auto md:max-w-[100%]",
     imageClassName: "aspect-[1.18/0.95]",
@@ -38,7 +38,7 @@ export const portfolioRightItems: PortfolioItem[] = [
   {
     title: "Narangba Timbers",
     image: "/images/portfolio/narangbatimber.jpg",
-    tags: ["App", "Interface"],
+    tags: ["WooCommerce", "Ecommerce"],
     href: "/portfolio/custom-mobile-app",
     wrapperClassName: "md:ml-auto md:max-w-[82%]",
     imageClassName: "aspect-[1/1]",
