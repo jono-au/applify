718c23d49b8422390c8aaa84f47a66e044e7d626
jono-au <jonathan@applify.com.au>
2026-04-02 12:57:15 +1000

    refactor add different size widths

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,17 +1,26 @@
 import type { ReactNode, HTMLAttributes } from "react";
+import clsx from "clsx";
 
 type ContainerProps = {
   children: ReactNode;
+  size?: "default" | "wide" | "narrow";
 } & HTMLAttributes<HTMLDivElement>;
 
 export default function Container({
   children,
-  className = "",
+  className,
+  size = "default",
   ...props
 }: ContainerProps) {
   return (
     <div
-      className={`mx-auto w-full max-w-[1440px] px-6 lg:px-10 ${className}`}
+      className={clsx(
+        "mx-auto w-full px-6 lg:px-10",
+        size === "narrow" && "max-w-[980px]",
+        size === "default" && "max-w-[1240px]",
+        size === "wide" && "max-w-[1440px]",
+        className,
+      )}
       {...props}
     >
       {children}
